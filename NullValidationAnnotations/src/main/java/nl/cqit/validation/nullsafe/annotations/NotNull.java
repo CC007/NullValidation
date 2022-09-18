bbf50aabb801f5c1a02ed70255d10d5557d62412
CC007 <Coolcat_the_best2@Hotmail.com>
2022-09-19 00:54:55 +0200

    Widening, narrowing and JavaDoc
    Signed-off-by: CC007 <Coolcat_the_best2@Hotmail.com>

--- NullValidationAnnotations/src/main/java/nl/cqit/validation/nullsafe/annotations/NotNull.java
+++ NullValidationAnnotations/src/main/java/nl/cqit/validation/nullsafe/annotations/NotNull.java
@@ -7,9 +7,27 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Specifies that the annotated field, record component, method parameter, method return value or local variable
+ * cannot contain a null value.
+ */
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.RECORD_COMPONENT, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 public @interface NotNull {
+
+    /**
+     * When a method is overridden by a subclass, this will specify if the method in the subclass can 
+     * widen (@{@link NotNull} -> @{@link Nullable}) the return value. 
+     * 
+     * When an annotated class implements an interface or extends another class, this will specify if the nullability 
+     * of the class itself can be widened.
+     *
+     * Widening is only applicable to annotated return values of overriding methods and annotated classes that extend 
+     * another class or implement an interface. It will be ignored on all other uses of the annotation.
+     * 
+     * Method parameters can ALWAYS be widened, regardless of this value, similar to how parameter types can be widened.
+     */
+    boolean allowWidening() default false;
 }
--- NullValidationAnnotations/src/main/java/nl/cqit/validation/nullsafe/annotations/Nullable.java
+++ NullValidationAnnotations/src/main/java/nl/cqit/validation/nullsafe/annotations/Nullable.java
@@ -7,9 +7,25 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Specifies that the annotated field, record component, method parameter, method return value or local variable
+ * can contain a null value.
+ */
 @Documented
 @Inherited
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.RECORD_COMPONENT, ElementType.METHOD, ElementType.PARAMETER, ElementType.LOCAL_VARIABLE})
 public @interface Nullable {
+
+    /**
+     * When a method is overridden by a subclass, this will specify if the method in the subclass can 
+     * narrow (@{@link Nullable} -> @{@link NotNull}) the method parameters. 
+     * 
+     * Narrowing is only applicable to method parameters and will be ignored on all other uses of the annotation.
+     * 
+     * Method return values can ALWAYS be narrowed, regardless of this value, similar to how parameter types can be narrowed.
+     *
+     * When an annotated class implements an interface or extends another class, it can always be narrowed.
+     */
+    boolean allowNarrowing() default false;
 }
