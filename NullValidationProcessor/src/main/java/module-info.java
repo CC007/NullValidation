64e837ce101b538dcd4cc60387d4348de8ea6f95
Rik Schaaf <schaaf.r@tkppensioen.nl>
2022-10-10 17:02:37 +0200

    use elementary instead of google compile testing

--- NullValidationProcessor/src/main/java/module-info.java
+++ NullValidationProcessor/src/main/java/module-info.java
@@ -2,4 +2,7 @@ module nl.cqit.validation.nullsafe.processor {
     requires nl.cqit.validation.nullsafe.annotations;
     requires com.google.auto.service;
     requires jdk.compiler;
+
+    exports nl.cqit.validation.nullsafe.processor;
+
 }
--- NullValidationProcessor/src/test/java/nl/cqit/validation/nullsafe/processor/internal/NotNullCheckerTest.java
+++ NullValidationProcessor/src/test/java/nl/cqit/validation/nullsafe/processor/internal/NotNullCheckerTest.java
@@ -1,34 +1,34 @@
 package nl.cqit.validation.nullsafe.processor.internal;
 
-import com.google.testing.compile.Compilation;
-import com.google.testing.compile.Compiler;
-import com.google.testing.compile.JavaFileObjects;
+import com.karuslabs.elementary.Results;
+import com.karuslabs.elementary.junit.JavacExtension;
+import com.karuslabs.elementary.junit.annotations.Classpath;
+import com.karuslabs.elementary.junit.annotations.Options;
+import com.karuslabs.elementary.junit.annotations.Processors;
+import com.karuslabs.elementary.junit.annotations.Resource;
 import nl.cqit.validation.nullsafe.processor.NotNullAnnotationProcessor;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-import static com.google.testing.compile.CompilationSubject.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
+@ExtendWith(JavacExtension.class)
+@Options(
+        "-d -.\\src\\test\\resources\\classes" +
+        " ---module-source-path -.\\src\\test\\resources" +
+        " ---module -test" +
+        " ---module-path -..\\NullValidationAnnotations\\target\\null-safe-annotations-1.0-SNAPSHOT.jar" +
+        " ---processor-path -..\\NullValidationAnnotations\\target\\null-safe-annotations-1.0-SNAPSHOT.jar")
+@Processors(NotNullAnnotationProcessor.class)
+@Resource("test/NullableClassWithNonNullMethod.java")
+@Resource("test/module-info.java")
 class NotNullCheckerTest {
 
     @Test
-    void checkReturnType() {
-        // prepare
-        final Compiler compiler = Compiler.javac()
-                .withOptions(
-                        "--module-source-path", ".\\src\\test\\resources",
-                        "--module", "test",
-                        "-d", ".\\src\\test\\resources\\classes",
-                        "--module-path", "..\\NullValidationAnnotations\\target\\null-safe-annotations-1.0-SNAPSHOT.jar",
-                        "--processor-path", "..\\NullValidationAnnotations\\target\\null-safe-annotations-1.0-SNAPSHOT.jar"
-                )
-                .withProcessors(new NotNullAnnotationProcessor());
-
-        // execute
-        Compilation compilation = compiler.compile(
-                JavaFileObjects.forResource("test/NullableClassWithNonNullMethod.java")
-        );
-
+    void checkReturnType(Results results) {
         // verify
-        assertThat(compilation).succeededWithoutWarnings();
+        results.find().errors().forEach(System.out::println);
+        assertThat(results.find().errors().count(), is(0));
     }
 }
